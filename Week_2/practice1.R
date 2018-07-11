2ae91a399d3ae5a3dc123c981473aec2331a9ab8
Lulu-Lee <40812788+Lulu-Lee@users.noreply.github.com>
2018-07-11 11:08:16 +0800

    practice1

---
+++ Week_2/practice1.R
@@ -0,0 +1,12 @@
+#install.packages("ggplot2")
+library(ggplot2)
+df <- data.frame(c("D0.5", "D1", "D2"), c(4.2, 10, 29.5))
+names(df) <- c("dose", "len")
+head(df)
+#Basic line plot with points
+ggplot(data=df, aes(x=dose, y=len, group=1)) + geom_line() + geom_point()
+#change the line type
+ggplot(data=df, aes(x=dose, y=len, group=1)) + geom_line(linetype = "dashed") + geom_point()
+#change the color
+ggplot(data=df, aes(x=dose, y=len, group=1)) + geom_line(color = "red") + geom_point()
+library(grid)
