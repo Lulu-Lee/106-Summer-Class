1e3e28195d3687af5f63db6a52c8c6aa3ab3093a
Lulu-Lee <40812788+Lulu-Lee@users.noreply.github.com>
2018-07-30 12:34:49 +0800

    cleaning

--- Week_2/practice2.R
+++ Week_2/practice2.R
@@ -6,3 +6,87 @@ pttTestFunction(URL[1], filename[1])
 mapply(pttTestFunction, 
        URL = URL, filename = filename)
 
+rm(list=ls(all.names = TRUE))
+library(NLP)
+library(tm)
+library(jiebaRD)
+library(jiebaR)
+library(RColorBrewer)
+library(wordcloud)
+filenames <- list.files(getwd(), pattern="*.txt")
+files <- lapply(filenames, readLines)
+docs <- Corpus(VectorSource(files))
+#移除可能有問題的符號
+toSpace <- content_transformer(function(x, pattern) {
+  return (gsub(pattern, " ", x))
+}
+)
+
+docs <- tm_map(docs, toSpace, "※")
+docs <- tm_map(docs, toSpace, "◆")
+docs <- tm_map(docs, toSpace, "‧")
+docs <- tm_map(docs, toSpace, "的")
+docs <- tm_map(docs, toSpace, "我")
+docs <- tm_map(docs, toSpace, "是")
+docs <- tm_map(docs, toSpace, "看板")
+docs <- tm_map(docs, toSpace, "作者")
+docs <- tm_map(docs, toSpace, "發信站")
+docs <- tm_map(docs, toSpace, "推")
+docs <- tm_map(docs, toSpace, "噓")
+docs <- tm_map(docs, toSpace, "很")
+docs <- tm_map(docs, toSpace, "了")
+docs <- tm_map(docs, toSpace, "不")
+docs <- tm_map(docs, toSpace, "會")
+docs <- tm_map(docs, toSpace, "可")
+docs <- tm_map(docs, toSpace, "以")
+docs <- tm_map(docs, toSpace, "好")
+docs <- tm_map(docs, toSpace, "應該")
+docs <- tm_map(docs, toSpace, "像")
+docs <- tm_map(docs, toSpace, "能")
+docs <- tm_map(docs, toSpace, "或")
+docs <- tm_map(docs, toSpace, "也")
+docs <- tm_map(docs, toSpace, "許")
+docs <- tm_map(docs, toSpace, "後")
+docs <- tm_map(docs, toSpace, "然")
+docs <- tm_map(docs, toSpace, "之")
+docs <- tm_map(docs, toSpace, "知道")
+docs <- tm_map(docs, toSpace, "一點")
+docs <- tm_map(docs, toSpace, "上")
+docs <- tm_map(docs, toSpace, "下")
+docs <- tm_map(docs, toSpace, "並")
+docs <- tm_map(docs, toSpace, "非")
+docs <- tm_map(docs, toSpace, "自")
+docs <- tm_map(docs, toSpace, "已")
+docs <- tm_map(docs, toSpace, "聽說")
+docs <- tm_map(docs, toSpace, "受")
+docs <- tm_map(docs, toSpace, "及")
+docs <- tm_map(docs, toSpace, "等")
+docs <- tm_map(docs, toSpace, "還")
+docs <- tm_map(docs, toSpace, "前")
+docs <- tm_map(docs, toSpace, "批踢踢實業坊")
+docs <- tm_map(docs, toSpace, "[a-zA-Z]")
+#移除標點符號 (punctuation)
+#移除數字 (digits)、空白 (white space)
+docs <- tm_map(docs, removePunctuation)
+docs <- tm_map(docs, removeNumbers)
+docs <- tm_map(docs, stripWhitespace)
+docs
+
+
+mixseg = worker()
+jieba_tokenizer=function(d){
+  unlist(segment(d[[1]],mixseg))
+}
+seg = lapply(docs, jieba_tokenizer)
+freqFrame = as.data.frame(table(unlist(seg)))
+freqFrame = freqFrame[order(freqFrame$Freq,decreasing=TRUE), ]
+library(knitr)
+kable(head(freqFrame), format = "markdown")
+
+
+wordcloud(freqFrame$Var1,freqFrame$Freq,
+          scale=c(5,0.1),min.freq=50,max.words=150,
+          random.order=TRUE, random.color=FALSE, 
+          rot.per=.1, colors=brewer.pal(8, "Dark2"),
+          ordered.colors=FALSE,use.r.layout=FALSE,
+          fixed.asp=TRUE)
